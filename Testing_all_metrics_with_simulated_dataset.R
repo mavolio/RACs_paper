e174e11c729a9c6a7802b0184c2cd08956fe7979
Meghan Avolio <meghanavolio@Meghans-MB.local>
2018-06-01 10:48:14 -0400

    new code for these aspects of the paper

---
+++ Testing_all_metrics_with_simulated_dataset.R
@@ -0,0 +1,482 @@
+library(tidyverse)
+library(vegan)
+library(gridExtra)
+library(grid)
+library(gtable)
+library(gtools)
+library(devtools)
+install_github("NCEAS/codyn", ref = github_pull(83))
+library(codyn)
+
+
+theme_set(theme_bw(12))
+
+# Read in Data ------------------------------------------------------------
+
+#simualted dataset
+sim<-df%>%#run rcommunity_simulating_communities.R then run RAC_scenarios, which creates df
+  mutate(time=as.numeric(iteration), 
+         id2=paste(id, site, sep="::"))%>%
+  select(-sample, -iteration)%>%
+  separate(id, into=c("alpha","theta","scenario","rep"), sep="_", remove=F)%>%
+  mutate(id3=paste(alpha, theta, scenario, sep="_"))
+
+# in the sim dataset, communites are differentiated by alpha (richness), theta (evenness) and scenario (rate of temporal and spatial variability: four scenarios: a: high temporal and high spatial variability; b: low temporal, low spatial variability; c: low temporal, high spatial variability; d: high temporal, low spatial variability"). For each richness-evennes combination (9 combinations) there are each community type. Each of these 10 community types have 10 replicates, called "sites" at a given point in time. Each community type, time, and site is then replicated 10 times.
+
+
+# Making tables for Appendix 5 -----------------------------------------
+
+###looking at temporal variability
+sim_turnover<-data.frame()
+com_rep<-unique(sim$id)
+
+for (i in 1:length(com_rep)){
+  
+  subset<-sim%>%
+    filter(id==com_rep[i])
+  
+  out <- turnover(df = subset, time.var = "time", species.var = "species", abundance.var = "abundance", replicate.var = "site")
+  
+  out$id<-com_rep[i]
+  
+  sim_turnover<-rbind(sim_turnover, out)  
+}
+
+sim_turnvoer_ave<-sim_turnover%>% 
+  group_by(id, time)%>%
+  summarise(total=mean(total))%>%
+  separate(id, into=c("alpha","theta","scenario","rep"), sep="_", remove=F)%>%
+  mutate(id3=paste(alpha, theta, scenario, sep="_"))%>%
+  group_by(id3, time)%>%
+  summarize(turnover=mean(total))%>%
+  ungroup()%>%
+  separate(id3, into=c("alpha","even","comtype"), sep="_")%>%
+  group_by(comtype)%>%
+  summarize(turnover=mean(turnover))
+
+### looking at spatial variability using Whittacker's beta diversity (gamma / average alpha)
+gammadiv<-sim%>%
+  filter(abundance!=0)%>%
+  group_by(id3, time, rep, species)%>%
+  summarize(splist=mean(abundance))%>%
+  ungroup()%>%
+  group_by(id3, time, rep)%>%
+  summarize(gamma=length(species))
+
+##richness
+S<-function(x){
+  x1<-x[x!=0]
+  length(x1)
+}
+
+rich <- group_by(sim, id3, time, rep, site) %>% 
+  summarize(S=S(abundance))%>%
+  ungroup()%>%
+  group_by(id3, time, rep)%>%
+  summarize(alpha=mean(S))
+
+beta_div<-gammadiv%>%
+  left_join(rich)%>%
+  mutate(wbeta=gamma/alpha)%>%
+  group_by(id3, time)%>%
+  summarize(wbeta=mean(wbeta))%>%
+  ungroup()%>%
+  separate(id3, into=c("alpha","even","comtype"), sep="_")%>%
+  group_by(comtype)%>%
+  summarize(betadiv=mean(wbeta))
+
+
+
+# Richness Evenness Metrics -----------------------------------------------
+com_rep<-unique(sim$id)
+
+sim_div_evar<-data.frame()
+for (i in 1:length(com_rep)){
+  
+  subset<-sim%>%
+    filter(id==com_rep[i])
+  
+  out <- community_structure(df = subset, time.var = "time", abundance.var = "abundance", replicate.var = "site", metric = "Evar")
+  out$id<-com_rep[i]
+  
+  sim_div_evar<-rbind(sim_div_evar, out)  
+}
+
+sim_diversity_mean<-sim_div_evar%>%
+  separate(id, into=c("alpha","theta","scenario","rep"), sep="_", remove=F)%>%
+  mutate(id3=paste(alpha, theta, scenario, sep="_"))%>%
+  group_by(id3, time, site)%>%#average over replicates
+  summarize(Sp=mean(richness),
+            Evar=mean(Evar, na.rm=T))%>%
+  ungroup()%>%
+  group_by(id3, time)%>%#average over sites
+  summarize(Sp=mean(Sp),
+            Evar=mean(Evar, na.rm=T))
+
+
+###making figure for appendix 5
+sim_div_tograph<-sim_diversity_mean%>%
+  separate(id3, into=c("alpha","theta","scenario"), sep="_", remove=F)
+
+ggplot(data = sim_div_tograph, aes(x = Sp, y = Evar, color = scenario))+
+  geom_point(size = 2)+
+  scale_color_brewer(type = "div", name = "Community Type", labels = c("High Spatial, High Temporal", "Low Spatial, Low Temporal", "High Spatial, Low Temporal", "Low Spatial, High Temporal"))+
+  xlab("Simulated Community Richness")+
+  ylab("Simulated Community Evenness (Evar)")+
+  theme(panel.grid = element_blank())
+
+
+
+# Change Metrics -------------------------------------------------------------
+
+### RAC Change
+sim_rac_change<-data.frame()
+
+com_rep<-unique(sim$id)
+
+for (i in 1:length(com_rep)){
+  
+  subset<-sim%>%
+    filter(id==com_rep[i])
+  
+  out <- RAC_change(df = subset, time.var = "time", species.var = "species", abundance.var = "abundance", replicate.var = "site")
+  
+  out$id<-com_rep[i]
+  
+  sim_rac_change<-rbind(sim_rac_change, out)  
+}
+
+sim_rac_change_mean<-sim_rac_change%>%
+  separate(id, into=c("alpha","theta","scenario","rep"), sep="_", remove=F)%>%
+  mutate(id3=paste(alpha, theta, scenario, sep="_"))%>%
+  group_by(id3, time, time2, site)%>%
+  summarize(S=mean(richness_change),
+            E=mean(evenness_change,na.rm=T),
+            R=mean(rank_change),
+            G=mean(gains),
+            L=mean(losses))%>%
+  ungroup()%>%
+  group_by(id3, time, time2)%>%
+  summarize(S=mean(S),
+            E=mean(E,na.rm=T),
+            R=mean(R),
+            G=mean(G),
+            L=mean(L))
+
+##Multivariate Change
+sim_mult_change<-data.frame()
+
+com_rep<-unique(sim$id)
+
+for (i in 1:length(com_rep)){
+  
+  subset<-sim%>%
+    filter(id==com_rep[i])
+  
+  out <- multivariate_change(df = subset, time.var = "time", species.var = "species", abundance.var = "abundance", replicate.var = "site")
+  
+  out$id<-com_rep[i]
+  
+  sim_mult_change<-rbind(sim_mult_change, out)  
+}
+
+sim_multchange_mean<-sim_mult_change%>%
+  separate(id, into=c("alpha","theta","scenario","rep"), sep="_", remove=F)%>%
+  mutate(id3=paste(alpha, theta, scenario, sep="_"))%>%
+  group_by(id3, time, time2)%>%
+  summarize(composition_change=mean(composition_change),
+            dispersion_change=mean(dispersion_change))
+
+## Curve change
+
+sim_curve_change<-data.frame()
+
+com_rep<-unique(sim$id)
+
+for (i in 1:length(com_rep)){
+  
+  subset<-sim%>%
+    filter(id==com_rep[i])
+  
+  out <- curve_change(df = subset, time.var = "time", species.var = "species", abundance.var = "abundance", replicate.var = "site")
+  
+  out$id<-com_rep[i]
+  
+  sim_curve_change<-rbind(sim_curve_change, out)  
+}
+
+sim_cc_ave<-sim_curve_change%>% 
+  group_by(id, time, time2)%>%
+  summarise(curve_change=mean(curve_change))%>%
+  separate(id, into=c("alpha","theta","scenario","rep"), sep="_", remove=F)%>%
+  mutate(id3=paste(alpha, theta, scenario, sep="_"))%>%
+  group_by(id3, time, time2)%>%
+  summarize(curve_change=mean(curve_change))
+
+
+# Difference Metrics ------------------------------------------------------
+
+sim_diff<-sim%>%
+  mutate(treatment = as.factor(ifelse(as.integer(site) <, "T1", "T2")))
+
+#RAC diff
+sim_rac_diff<-data.frame()
+com_rep<-unique(sim_diff$id)
+
+for (i in 1:length(com_rep)){
+  
+  subset<-sim_diff%>%
+    filter(id==com_rep[i])
+  
+  out <- RAC_difference(df = subset, time.var = "time", species.var = "species", abundance.var = "abundance", replicate.var = "site", pool = TRUE, treatment.var = "treatment")
+  
+  out$id<-com_rep[i]
+  
+  sim_rac_diff<-rbind(sim_rac_diff, out)  
+}
+
+sim_rac_diff_mean<-sim_rac_diff%>%
+  separate(id, into=c("alpha","theta","scenario","rep"), sep="_", remove=F)%>%
+  mutate(id3=paste(alpha, theta, scenario, sep="_"))%>%
+  group_by(id3, time)%>%
+  summarize(S=mean(richness_diff),
+            E=mean(evenness_diff,na.rm=T),
+            R=mean(rank_diff),
+            D=mean(species_diff))
+
+##multivariate_diff
+sim_mult_diff<-data.frame()
+com_rep<-unique(sim_diff$id)
+
+for (i in 1:length(com_rep)){
+  
+  subset<-sim_diff%>%
+    filter(id==com_rep[i])
+  
+  out <- multivariate_difference(df = subset, time.var = "time", species.var = "species", abundance.var = "abundance", replicate.var = "site", treatment.var = "treatment")
+  
+  out$id<-com_rep[i]
+  sim_mult_diff<-rbind(sim_mult_diff, out)  
+}
+
+sim_mult_diff_mean<-sim_mult_diff%>%
+  separate(id, into=c("alpha","theta","scenario","rep"), sep="_", remove=F)%>%
+  mutate(id3=paste(alpha, theta, scenario, sep="_"))%>%
+  group_by(id3, time)%>%
+  summarize(composition_diff=mean(composition_diff),
+            abs_dispersion_diff=mean(abs_dispersion_diff))
+
+###curve diff
+sim_curve_diff<-data.frame()
+com_rep<-unique(sim_diff$id)
+
+for (i in 1:length(com_rep)){
+  subset<-sim_diff%>%
+    filter(id==com_rep[i])
+  
+  out <- curve_difference(df = subset, time.var = "time", species.var = "species", abundance.var = "abundance", replicate.var = "site", pool = TRUE, treatment.var = "treatment")
+  
+  out$id<-com_rep[i]
+  sim_curve_diff<-rbind(sim_curve_diff, out)  
+}
+
+sim_cc_diff<-sim_curve_diff%>% 
+  separate(id, into=c("alpha","theta","scenario","rep"), sep="_", remove=F)%>%
+  mutate(id3=paste(alpha, theta, scenario, sep="_"))%>%
+  group_by(id3, time)%>%
+  summarize(curve_diff=mean(curve_diff))
+
+sim_diff_div<-sim_div_evar%>%
+  separate(id, into=c("alpha","theta","scenario","rep"), sep="_", remove=F)%>%
+  mutate(id3=paste(alpha, theta, scenario, sep="_"))%>%
+  group_by(id3, time, site)%>%#average over replicates
+  summarize(Sp=mean(richness),
+            Evar=mean(Evar, na.rm=T))%>%
+  ungroup()%>%
+  group_by(id3, time)%>%#average over sites
+  summarize(Sp=mean(Sp),
+            Evar=mean(Evar, na.rm=T))
+
+# Merging all metrics to single datasets ----------------------------------
+
+sim_allmetrics<-sim_multchange_mean%>%
+  left_join(sim_rac_change_mean)%>%
+  left_join(sim_cc_ave)%>%
+  left_join(sim_diversity_mean)%>%
+  separate(id3, into=c("alpha","even","comtype"), sep="_")%>%
+  mutate(comtype2 = as.factor(comtype))
+
+
+sim_diff_allmetrics<-sim_rac_diff_mean%>%
+  left_join(sim_cc_diff)%>%
+  left_join(sim_mult_diff_mean)%>%
+  left_join(sim_diff_div)%>%
+  separate(id3, into=c("alpha","even","comtype"), sep="_")%>%
+  mutate(comtype2 = as.factor(comtype))
+
+# Effect of rich and even on CHANGE and DIFFERNCE sim data Table 2 ------------------------------
+
+#How are CHANGE metrics affected by the richness and evenness of the community?
+#Richness
+#rich with delta rank
+with(sim_allmetrics,  cor.test(Sp, R))
+
+##removing extra division by Sp to demonstate to why it is necessary to divide again by the size of the species pool.
+sim_allmetrics2<-sim_allmetrics%>%
+  mutate(MRS = R*Sp)
+with(sim_allmetrics2, cor.test(Sp, MRS))
+
+ggplot(data=sim_allmetrics2, aes(x=Sp, y=MRS, color = Evar))+
+  geom_point()+
+  xlab("Simulated Community Richness")+
+  ylab("Rank Change")+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
+  facet_wrap(~comtype, labeller=labeller(comtype = labels), ncol =4)
+
+#rich with gains or lossess same thing
+with(sim_allmetrics, cor.test(Sp, G))
+
+#rich with composition
+with(sim_allmetrics, cor.test(Sp, composition_change))
+
+#rich with dispersion
+with(sim_allmetrics, cor.test(Sp, dispersion_change))
+
+#rich with curve
+with(sim_allmetrics, cor.test(Sp, curve_change))
+
+#even with delta rank
+with(sim_allmetrics, cor.test(Evar, R))
+
+#even with gains/losses
+with(sim_allmetrics, cor.test(Evar, G))
+
+#even with composition
+with(sim_allmetrics, cor.test(Evar, composition_change))
+
+#even with dispersion
+with(sim_allmetrics, cor.test(Evar, dispersion_change))
+
+#even with curve
+with(sim_allmetrics, cor.test(Evar, curve_change))
+
+###doing the correlations for the difference metrics
+
+#rich with delta rank
+with(sim_diff_allmetrics, cor.test(Sp, R))
+
+#rich with species_diff
+with(sim_diff_allmetrics,cor.test(Sp, D))
+
+#rich with composition
+with(sim_diff_allmetrics, cor.test(Sp, composition_diff))
+
+#rich with dispersion
+with(sim_diff_allmetrics, cor.test(Sp, abs_dispersion_diff))
+
+#rich with curve
+with(sim_diff_allmetrics, cor.test(Sp, curve_diff))
+
+#even with delta rank
+with(sim_diff_allmetrics, cor.test(Evar, R))
+
+#even with species difference
+with(sim_diff_allmetrics, cor.test(Evar, D))
+
+#even with composition
+with(sim_diff_allmetrics, cor.test(Evar, composition_diff))
+
+#even with dispersion
+with(sim_diff_allmetrics, cor.test(Evar, abs_dispersion_diff))
+
+#even with curve
+with(sim_diff_allmetrics, cor.test(Evar, curve_diff))
+
+
+# Looking at figures of rich/even on metrics Appendix 6 figures and Table -----------
+
+
+#Change metrics
+sim_tograph<-sim_allmetrics%>%
+  gather(metric, value, composition_change:curve_change)%>%
+  filter(metric != "S")%>%
+  filter(metric != "E")%>%
+  filter(metric != "L")
+
+
+labels_change <-c(composition_change = "Composition Change",
+           curve_change = "Curve Change",
+           dispersion_change = "Dispersion Change",
+           G = "Gains/Losses",
+           R = "Rank Change")
+
+#change with richness
+ggplot(data=sim_tograph, aes(x=as.factor(Sp), y=value, color = comtype))+
+  geom_boxplot()+
+  scale_color_manual(name = "Community Type", labels = c("High Spatial, High Temporal","Low Spatial, Low Temporal","High Spatail, Low Temporal","Low Spatial, High Temporal"), values= c("gold","orange","darkorange4","red"))+
+  xlab("Simulated Community Richness")+
+  ylab("Metric Value")+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
+  facet_wrap(~metric, ncol = 3, scales = "free", labeller = labeller(metric = labels_change))+
+  theme(strip.background = element_rect(fill = 0))
+
+
+#change with evenness
+ggplot(data=sim_tograph, aes(x=as.factor(even), y=value, color = comtype))+
+  geom_boxplot()+
+  scale_color_manual(name = "Community Type", labels = c("High Spatial, High Temporal","Low Spatial, Low Temporal","High Spatail, Low Temporal","Low Spatial, High Temporal"), values= c("gold","orange","darkorange4","red"))+
+  xlab("Simulated Community Evenness")+
+  ylab("Metric Value")+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
+  facet_wrap(~metric, ncol = 3, scales = "free", labeller = labeller(metric = labels_change))+
+  theme(strip.background = element_rect(fill = 0))+
+  scale_x_discrete(labels = c("Low","Mid","High"))
+
+sim_means<-sim_allmetrics%>%
+  mutate(abs_dc = abs(dispersion_change))%>%
+  gather(metric, value, c(composition_change:curve_change, abs_dc))%>%
+  group_by(comtype, metric)%>%
+  summarize(mean = mean (value),
+            se = sd(value)/sqrt(81))
+
+
+
+###Difference Metrics
+
+sim_diff_tograph<-sim_diff_allmetrics%>%
+  gather(metric, value, S:abs_dispersion_diff)%>%
+  filter(metric != "S")%>%
+  filter(metric != "E")
+
+labels_diff <-c(composition_diff = "Composition Difference",
+                  curve_diff = "Curve Difference",
+                  abs_dispersion_diff = "Dispersion Difference",
+                  D = "Species Differences",
+                  R = "Rank Difference")
+#diff with richness
+ggplot(data=sim_diff_tograph, aes(x=as.factor(Sp), y=value, color = comtype))+
+  geom_boxplot()+
+  scale_color_manual(name = "Community Type", labels = c("High Spatial, High Temporal","Low Spatial, Low Temporal","High Spatail, Low Temporal","Low Spatial, High Temporal"), values= c("gold","orange","darkorange4","red"))+
+  xlab("Simulated Community Richness")+
+  ylab("Metric Value")+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
+  facet_wrap(~metric, ncol = 3, scales = "free", labeller = labeller(metric = labels_diff))+
+  theme(strip.background = element_rect(fill = 0))
+
+
+#diff with evenness
+ggplot(data=sim_diff_tograph, aes(x=as.factor(even), y=value, color = comtype))+
+  geom_boxplot()+
+  scale_color_manual(name = "Community Type", labels = c("High Spatial, High Temporal","Low Spatial, Low Temporal","High Spatail, Low Temporal","Low Spatial, High Temporal"), values= c("gold","orange","darkorange4","red"))+
+  xlab("Simulated Community Evenness")+
+  ylab("Metric Value")+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
+  facet_wrap(~metric, ncol = 3, scales = "free", labeller = labeller(metric = labels_diff))+
+  theme(strip.background = element_rect(fill = 0))+
+  scale_x_discrete(labels = c("Low","Mid","High"))
+
+diff_means<-sim_diff_tograph%>%
+  group_by(comtype, metric)%>%
+  summarize(mean = mean (value),
+            se = sd(value)/sqrt(90))
+
---
+++ Testing_change_metrics_with_CommDyn_dataset.R
@@ -0,0 +1,275 @@
+library(tidyverse)
+library(vegan)
+library(gridExtra)
+library(grid)
+library(gtable)
+#library(gtools)
+library(devtools)
+install_github("NCEAS/codyn", ref = github_pull(83))
+library(codyn)
+
+# Read in Data ------------------------------------------------------------
+
+#community dynamics dataset
+
+##read in full dataset
+infile1  <- "https://pasta.lternet.edu/package/data/eml/edi/15/5/f69c8fe563067164191d61b6e33eff03" 
+infile1 <- sub("^https","http",infile1) 
+dt1 <-read.csv(infile1,header=F 
+               ,skip=1
+               ,sep=","  
+               ,quot='"' 
+               , col.names=c(
+                 "rowID",     
+                 "sitesubplot",     
+                 "experiment_year",     
+                 "site_code",     
+                 "project_name",     
+                 "community_type",     
+                 "site_project_comm",     
+                 "plot_id",     
+                 "species",     
+                 "relcover"    ), check.names=TRUE)
+
+
+# Fix any interval or ratio columns mistakenly read in as nominal and nominal columns read as numeric or dates read as strings
+
+if (class(dt1$rowID)!="factor") dt1$rowID<- as.factor(dt1$rowID)
+if (class(dt1$sitesubplot)!="factor") dt1$sitesubplot<- as.factor(dt1$sitesubplot)
+if (class(dt1$experiment_year)=="factor") dt1$experiment_year <-as.numeric(levels(dt1$experiment_year))[as.integer(dt1$experiment_year) ]
+if (class(dt1$site_code)!="factor") dt1$site_code<- as.factor(dt1$site_code)
+if (class(dt1$project_name)!="factor") dt1$project_name<- as.factor(dt1$project_name)
+if (class(dt1$community_type)!="factor") dt1$community_type<- as.factor(dt1$community_type)
+if (class(dt1$site_project_comm)!="factor") dt1$site_project_comm<- as.factor(dt1$site_project_comm)
+if (class(dt1$plot_id)!="factor") dt1$plot_id<- as.factor(dt1$plot_id)
+if (class(dt1$species)!="factor") dt1$species<- as.factor(dt1$species)
+if (class(dt1$relcover)=="factor") dt1$relcover <-as.numeric(levels(dt1$relcover))[as.integer(dt1$relcover) ]
+
+
+codyndat<-dt1%>%
+  filter(site_code!="MISS")
+
+
+##read in site and experiment information
+infile2  <- "https://pasta.lternet.edu/package/data/eml/edi/15/5/8284876afe3a1cb0a919d37e1164357f" 
+infile2 <- sub("^https","http",infile2) 
+dt2 <-read.csv(infile2,header=F 
+               ,skip=1
+               ,sep=","  
+               ,quot='"' 
+               , col.names=c(
+                 "site_code",     
+                 "site_project_comm",     
+                 "project_name",     
+                 "community_type",     
+                 "location",     
+                 "Country",     
+                 "Continent",     
+                 "Lat",     
+                 "Long",     
+                 "MAP_mm",     
+                 "plot_size",     
+                 "spatial_extent",     
+                 "succession",     
+                 "lifespan",     
+                 "trophic_level",     
+                 "taxa",     
+                 "ANPP",     
+                 "broad_ecosystem_type",     
+                 "num_plots",     
+                 "temp_C",     
+                 "dataset_length",     
+                 "time_step"    ), check.names=TRUE)
+
+
+# Fix any interval or ratio columns mistakenly read in as nominal and nominal columns read as numeric or dates read as strings
+
+if (class(dt2$site_code)!="factor") dt2$site_code<- as.factor(dt2$site_code)
+if (class(dt2$site_project_comm)!="factor") dt2$site_project_comm<- as.factor(dt2$site_project_comm)
+if (class(dt2$project_name)!="factor") dt2$project_name<- as.factor(dt2$project_name)
+if (class(dt2$community_type)!="factor") dt2$community_type<- as.factor(dt2$community_type)
+if (class(dt2$location)!="factor") dt2$location<- as.factor(dt2$location)
+if (class(dt2$Country)!="factor") dt2$Country<- as.factor(dt2$Country)
+if (class(dt2$Continent)!="factor") dt2$Continent<- as.factor(dt2$Continent)
+if (class(dt2$Lat)=="factor") dt2$Lat <-as.numeric(levels(dt2$Lat))[as.integer(dt2$Lat) ]
+if (class(dt2$Long)=="factor") dt2$Long <-as.numeric(levels(dt2$Long))[as.integer(dt2$Long) ]
+if (class(dt2$MAP_mm)=="factor") dt2$MAP_mm <-as.numeric(levels(dt2$MAP_mm))[as.integer(dt2$MAP_mm) ]
+if (class(dt2$plot_size)=="factor") dt2$plot_size <-as.numeric(levels(dt2$plot_size))[as.integer(dt2$plot_size) ]
+if (class(dt2$spatial_extent)=="factor") dt2$spatial_extent <-as.numeric(levels(dt2$spatial_extent))[as.integer(dt2$spatial_extent) ]
+if (class(dt2$succession)!="factor") dt2$succession<- as.factor(dt2$succession)
+if (class(dt2$lifespan)!="factor") dt2$lifespan<- as.factor(dt2$lifespan)
+if (class(dt2$trophic_level)!="factor") dt2$trophic_level<- as.factor(dt2$trophic_level)
+if (class(dt2$taxa)!="factor") dt2$taxa<- as.factor(dt2$taxa)
+if (class(dt2$ANPP)=="factor") dt2$ANPP <-as.numeric(levels(dt2$ANPP))[as.integer(dt2$ANPP) ]
+if (class(dt2$broad_ecosystem_type)!="factor") dt2$broad_ecosystem_type<- as.factor(dt2$broad_ecosystem_type)
+if (class(dt2$num_plots)=="factor") dt2$num_plots <-as.numeric(levels(dt2$num_plots))[as.integer(dt2$num_plots) ]
+if (class(dt2$temp_C)=="factor") dt2$temp_C <-as.numeric(levels(dt2$temp_C))[as.integer(dt2$temp_C) ]
+if (class(dt2$dataset_length)=="factor") dt2$dataset_length <-as.numeric(levels(dt2$dataset_length))[as.integer(dt2$dataset_length) ]
+if (class(dt2$time_step)=="factor") dt2$time_step <-as.numeric(levels(dt2$time_step))[as.integer(dt2$time_step) ]
+
+codyndat_info<-dt2
+
+###CLEANING CODYN DATASET
+#restrict to species that are present in an experiment
+splist<-codyndat%>%
+  group_by(site_code, project_name, community_type, species)%>%
+  summarize(present=sum(relcover))%>%
+  filter(present!=0)%>%
+  select(-present)
+
+#merge back and will drop species that do not exist in a dataset
+codyndat_clean<-merge(codyndat, splist, by=c("site_code","project_name","community_type","species"))%>%
+  select(-site_project_comm)%>%
+  mutate(site_project_comm = paste(site_code, project_name, community_type, sep = "_"))%>%
+  select(-sitesubplot, -site_code, -project_name, -community_type)%>%
+  mutate(id=paste(site_project_comm, plot_id, sep="::"))
+
+
+
+# Richness Evenness Metrics -----------------------------------------------
+
+codyn_div_evar<-data.frame()
+
+for (i in 1:length(spc)){
+  subset<-codyndat_clean%>%
+    filter(site_project_comm==spc[i])
+  
+  out<-community_structure(subset, time.var = 'experiment_year', abundance.var = 'relcover', replicate.var = 'plot_id', metric = "Evar")
+  out$site_project_comm<-spc[i]
+  
+  codyn_div_evar<-rbind(codyn_div_evar, out)
+}
+
+
+codyndat_diversity_mean <- codyn_div_evar%>%
+  group_by(site_project_comm, experiment_year)%>%
+  summarize(Sp=mean(richness),
+            Evar=mean(Evar, na.rm=T))
+
+# RAC changes -------------------------------------------------------------
+
+codyndat_rac_change<-data.frame()
+spc<-unique(codyndat_clean$site_project_comm)
+
+for (i in 1:length(spc)){
+  
+  subset<-codyndat_clean%>%
+    filter(site_project_comm==spc[i])
+  
+  out <- RAC_change(df = subset, time.var = "experiment_year", species.var = "species", abundance.var = "relcover", replicate.var = "plot_id")
+  
+  out$site_project_comm<-spc[i]
+  
+  codyndat_rac_change<-rbind(codyndat_rac_change, out)  
+}
+
+codyndat_rac_change_average<-codyndat_rac_change%>%
+  group_by(site_project_comm, experiment_year, experiment_year2)%>%
+  summarise(S=mean(richness_change),
+            E=mean(evenness_change,na.rm=T),
+            R=mean(rank_change),
+            G=mean(gains),
+            L=mean(losses))
+
+# Mean Change and Dispersion ----------------------------------------------
+codyn_multchange<-data.frame()
+spc<-unique(codyndat_clean$site_project_comm)
+
+for (i in 1:length(spc)){
+  
+  subset<-codyndat_clean%>%
+    filter(site_project_comm==spc[i])
+  
+  out <- multivariate_change(df = subset, time.var = "experiment_year", species.var = "species", abundance.var = "relcover", replicate.var = "plot_id")
+  
+  out$site_project_comm<-spc[i]
+
+  codyn_multchange<-rbind(codyn_multchange, out)  
+}
+
+# Curve change ------------------------------------------------------------
+
+codyn_curvechange<-data.frame()
+spc<-unique(codyndat_clean$site_project_comm)
+
+for (i in 1:length(spc)){
+  
+  subset<-codyndat_clean%>%
+    filter(site_project_comm==spc[i])
+  
+  out <- curve_change(df = subset, time.var = "experiment_year", species.var = "species", abundance.var = "relcover", replicate.var = "plot_id")
+  
+  out$site_project_comm<-spc[i]
+  
+  codyn_curvechange<-rbind(codyn_curvechange, out)  
+}
+
+codyn_curvechange_mean<-codyn_curvechange%>% 
+  group_by(site_project_comm, experiment_year, experiment_year2)%>%
+  summarise(curve_change=mean(curve_change))
+
+# Merging all metrics to single datasets ----------------------------------
+
+####MERGING TO A SINGE DATASET
+
+codyndat_allmetrics<-codyn_multchange%>%
+  left_join(codyndat_rac_change_average)%>%
+  left_join(codyn_curvechange_mean)%>%
+  left_join(codyndat_diversity_mean)%>%
+  left_join(codyndat_info)
+
+
+# Making figure 4 --------------------------------------------------------
+
+theme_set(theme_bw(12))
+
+codyndat_allmetrics<-codyndat_allmetrics%>%
+  mutate(absS = abs(S),
+         absE = abs(E))
+
+#Code to make pairs graph
+panel.cor <- function(x, y, digits = 2, prefix = "", cex.cor, ...){
+  usr <- par("usr"); on.exit(par(usr))
+  par(usr = c(0, 1, 0, 1))
+  r <- cor(x, y)
+  txt <- format(c(r, 0.123456789), digits = digits)[1]
+  txt <- paste0(prefix, txt)
+  if(missing(cex.cor)) cex.cor <- 0.8/strwidth(txt)
+  test <- cor.test(x,y) 
+  Signif <- symnum(test$p.value, corr = FALSE, na = FALSE, 
+                   cutpoints = c(0, 0.001, 1),
+                   symbols = c("*", " "))
+  
+  
+  text(0.5, 0.5, txt, cex = 2)
+  text(0.8, 0.5, Signif, cex=5, col="red")
+}
+
+panel.hist <- function(x, ...)
+{
+  usr <- par("usr"); on.exit(par(usr))
+  par(usr = c(usr[1:2], 0, 1.5) )
+  h <- hist(x, plot = FALSE)
+  breaks <- h$breaks; nB <- length(breaks)
+  y <- h$counts; y <- y/max(y)
+  rect(breaks[-nB], 0, breaks[-1], y, ...)
+}
+
+colnames(codyndat_allmetrics)# double check columns are correct
+#comparing absolute S and E changes
+pairs(codyndat_allmetrics[,c(35,36, 8:10, 3, 4, 11)], col=codyndat_allmetrics$taxa,upper.panel = panel.cor,diag.panel = panel.hist, cex.axis = 2)
+par(xpd=T)
+
+
+
+# Make table 3 --------
+
+#how do these correlate with experiment parameters.
+codyndat_allmetrics3<-codyndat_allmetrics%>%
+  group_by(site_project_comm)%>%
+  summarize_at(vars(absS, absE, R, G, L, curve_change, composition_change, dispersion_change, spatial_extent, plot_size, num_plots), funs(mean), rm.na=T)%>%
+  mutate(spatialExtent=log(spatial_extent),plotSize=log(plot_size))
+                 
+colnames(codyndat_allmetrics3)
+pairs(codyndat_allmetrics3[,c(2:9, 12:14)], upper.panel = panel.cor)
+cor(codyndat_allmetrics3[,c(2:9, 12:14)], method = "pearson")
